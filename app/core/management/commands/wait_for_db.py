f86dcc98c902043d61e93a73ac5148597a5d7cdb
asDev-Jorge <janza@premiumsystems.com.mx>
2023-03-07 09:45:24 -0600

    Se agregan un salto de linea

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,19 +1,13 @@
 """
 Django commad to wait for the database to be available.    
 """
-
 import time
-
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
     
-    
-    
 class Command(BaseCommand):    
     """Django command to wait for database."""
-
-
     def handle(self, *args,**options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
@@ -26,4 +20,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
